4e4b5b5e1fcb289326419b57d3a628fa05b53a77
mbjoshi44 <119665891+mbjoshi44@users.noreply.github.com>
2023-09-13 13:53:54 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
 var clutter = "";
+var timer = 60;
+var hitRN=0;
+var scoreNum = 0;
 
-for(let i = 1;i<=102;i++){
-    clutter += `<div class="bubble">5</div>`
+alert("match  number with hit value and get point ")
+
+
+const createBubble =()=>{
+
+    for(let i = 1;i<=54;i++){
+        var rn = Math.floor(Math.random()*10)
+        clutter += `<div class="bubble">${rn}</div>`
+    }
+       document.querySelector("#pbtm").innerHTML = clutter
 }
 
-document.querySelector("#pbtm").innerHTML = clutter
+const createTimer =()=>{
+ var timerint= setInterval(()=>{
+    if(timer>0){
+        timer --;
+        document.querySelector("#timerVal").textContent = timer
+    }else{
+        clearInterval(timerint)
+        document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`
+    }
+ },1000)
+}
+
+const setHit =()=>{
+    hitRN  = Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent = hitRN
+}
+
+const setScore =()=>{    
+    scoreNum +=10;
+    document.querySelector("#scoreVal").textContent=scoreNum;
+}
+
+const setScoreNegative =()=>{    
+    scoreNum -=5;
+    document.querySelector("#scoreVal").textContent=scoreNum;
+}
+
+document.querySelector("#pbtm").addEventListener("click", (e)=>{
+      var num = Number(e.target.textContent);
+      if(num === hitRN){
+        setScore()
+        clutter = "";
+        createBubble()
+        setHit()
+      }else{
+        setScoreNegative()
+      }
+})
+
+createTimer()
+createBubble()
+setHit()
